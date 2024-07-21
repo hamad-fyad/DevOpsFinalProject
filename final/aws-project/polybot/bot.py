0e948fc2ea9af870ce4a732135a2b40775248275
hamad <Hamad.fyad.05@gmail.com>
2024-07-21 17:06:23 +0300

    test 59

--- final/aws-project/polybot/bot.py
+++ final/aws-project/polybot/bot.py
@@ -1,133 +1,71 @@
-import base64
-import io
 import json
+
 import requests
 import telebot
+from botocore.exceptions import ClientError
 from loguru import logger
 import os
 import time
-import boto3
 from telebot.types import InputFile
-from botocore.exceptions import ClientError
+import boto3
 
-# def get_k8s_secret1(namespace, secret_name):
-#         # Load kubeconfig from default location or specified path
-#         config.load_kube_config()  # Use config.load_incluster_config() if running inside a pod
 
-#         # Create an instance of the CoreV1Api
-#         v1 = client.CoreV1Api()
+def get_secret():
+    secret_name = "hamad-pixabay-token"
+    region_name = "eu-north-1"
 
-#         try:
-#             # Retrieve the secret
-#             secret = v1.read_namespaced_secret(secret_name, namespace)
-#             return secret.data
-#         except client.exceptions.ApiException as e:
-#             print(f"Exception when calling CoreV1Api->read_namespaced_secret: {e}")
-#             return None
-        
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+    
+
+    secret_data = json.loads(secret)
+    
+    # Access the value associated with the 'pixabay_token' key
+    telebot_value = secret_data['pixabay_token']
+    
+    return telebot_value
+
+
+
 class Bot:
-    
-    
-    
 
     def __init__(self, token, telegram_chat_url):
-        print(telegram_chat_url)
-        print(token)
-
         # create a new instance of the TeleBot class.
         # all communication with Telegram servers are done using self.telegram_bot_client
         self.telegram_bot_client = telebot.TeleBot(token)
-        #TODO put the certificate in the k8s secret and pull from there and change the code here
+
         # remove any existing webhooks configured in Telegram servers
         self.telegram_bot_client.remove_webhook()
         time.sleep(0.5)
-        # namespace = "hamad"  # Replace with your namespace
-        # secret_name = "tls-secret-hamad"  # Replace with your secret name
-        # secret = get_k8s_secret1(namespace, secret_name)
-        # Create a file-like object from the PEM string
-        # print("##################################1####################################")
-        # print(secret)
-        # print("################################2######################################")
-        # pem_file = io.StringIO(secret)
-        # print("###############################3#######################################")
-        # print(type(pem_file))
-        # print("###############################4#######################################")
-        # print(pem_file)
-        # print("###############################5#######################################")
 
-        # #self.telegram_bot_client.set_webhook(url=f'{telegram_chat_url}/{token}/', timeout=60)
-        # response = self.telegram_bot_client.set_webhook(url=f'{telegram_chat_url}/{token}/', certificate=open(pem_file,'r'))#TODO check if it works it may not 
+        # todo set the webhook URL change path in ec2 docker container
+        self.telegram_bot_client.set_webhook(url=f'{telegram_chat_url}/{token}/',
+                                             certificate=open('YOURPUBLIC.pem', 'r'),
+                                             timeout=60)
 
-        # print(response)
-        # logger.info(f'Telegram Bot information\n\n{self.telegram_bot_client.get_me()}')
-        # with open('', 'r') as file: # TODO pull the certificate from the k8s secret
-        #   pem_contents = file.read()
-        #   print(pem_contents)
-        # set the webhook URL
-        # session = boto3.session.Session()
-        # client = session.client(
-        #     service_name='secretsmanager',
-        #     region_name="eu-north-1"
-        # )
-        # secret_response  = client.get_secret_value(
-        #     SecretId="hamad-telegram-cert"
-        # )
-
-        # pem_contents = secret_response['SecretString']
-        # print(pem_contents)
-        # # set the webhook URL
-        # # Create a file-like object from the PEM string
-        # pem_file = io.StringIO(pem_contents)
-        # #self.telegram_bot_client.set_webhook(url=f'{telegram_chat_url}/{token}/', timeout=60)
-        # response = self.telegram_bot_client.set_webhook(url=f'{telegram_chat_url}/{token}/', certificate=pem_file)
-        # print(response)
-        # logger.info(f'Telegram Bot information\n\n{self.telegram_bot_client.get_me()}')
-        client = boto3.session.Session().client(
-            service_name='secretsmanager',
-            region_name="eu-north-1"
-        )
-        secret_response = client.get_secret_value(
-            SecretId="hamad-telegram-cert"
-        )
-        pem_contents = secret_response['SecretString']
-        json_contents = json.loads(pem_contents)
-        # print(json_contents["hamad-cert"])
-        secret = io.StringIO(json_contents["hamad-cert"].replace(" ", "\n").replace("-----BEGIN\nCERTIFICATE-----", "-----BEGIN CERTIFICATE-----").replace("-----END\nCERTIFICATE-----", "-----END CERTIFICATE-----"))
-        # set the webhook URL
-        # Create a file-like object from the PEM string
-        #self.telegram_bot_client.set_webhook(url=f'{telegram_chat_url}/{token}/', timeout=60)
-        print("##################################1####################################")
-        print(secret.read())
-        print("################################2######################################")
-        response = self.telegram_bot_client.set_webhook(url=f'{telegram_chat_url}/{token}/', certificate=secret.read())
-        print(response)
         logger.info(f'Telegram Bot information\n\n{self.telegram_bot_client.get_me()}')
-    
-
     def send_text(self, chat_id, text):
         self.telegram_bot_client.send_message(chat_id, text)
-    
-    def send_text_result(self, chat_id, desc_json):
-        class_counts = {}
-        for item in desc_json:
-            class_name = item.get("class")
-            if class_name:
-                class_counts[class_name] = class_counts.get(class_name, 0) + 1
-    
-        message = ""
-        for class_name, count in class_counts.items():
-            message += f"{class_name}: {count}\n"
-        
-        self.telegram_bot_client.send_message(chat_id, message)
-      
-        
-    
+
     def send_text_with_quote(self, chat_id, text, quoted_msg_id):
         self.telegram_bot_client.send_message(chat_id, text, reply_to_message_id=quoted_msg_id)
-        
 
-    @staticmethod
-    def is_current_msg_photo(msg):
+    def is_current_msg_photo(self, msg):
         return 'photo' in msg
 
     def download_user_photo(self, msg):
@@ -154,81 +92,122 @@ class Bot:
         if not os.path.exists(img_path):
             raise RuntimeError("Image path doesn't exist")
 
-        self.telegram_bot_client.send_photo(chat_id,InputFile(img_path))
+        self.telegram_bot_client.send_photo(
+            chat_id,
+            InputFile(img_path)
+        )
 
     def handle_message(self, msg):
         """Bot Main message handler"""
         logger.info(f'Incoming message: {msg}')
-        if 'text' in msg:
-            self.send_text(msg['chat']['id'], f'3 :Your original message: {msg["text"]}')
+        self.send_text(msg['chat']['id'], f'Your original message: {msg["text"]}')
+
+
+# pixabay_token = os.environ['PIXABAY_TOKEN']
+def get_photo(param: str):
+    param.replace(" ", "")
+    pixabay_token = get_secret()
+    url = f"https://pixabay.com/api/?key={pixabay_token}&q={param[8:].lower()}&image_type=photo"
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            first_large_image_url = ""
+            for hit in data["hits"]:
+                if "largeImageURL" in hit:
+                    logger.info(f"check {hit['largeImageURL']}")
+                    first_large_image_url = hit["largeImageURL"]
+                    break
+            if first_large_image_url:
+                return first_large_image_url
+            else:
+                logger.info("No images found.")
+                return "No images found."
         else:
-            self.send_text(msg['chat']['id'], "Sorry, I couldn't process your message.")
+            logger.error(f"Failed to fetch data: HTTP Status Code {response.status_code}")
+            return f"Error: Unable to access Pixabay API, received status code {response.status_code}"
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
+        return "Error: An issue occurred while attempting to fetch the photo."
 
 
-class QuoteBot(Bot):
-    def handle_message(self, msg):
-        logger.info(f'Incoming message: {msg}')
+def get_info_of_currency(currency: str):
+    currency.replace(" ", "")
+    try:
+        url = f"https://api.coincap.io/v2/assets/{currency}"
+        data = requests.get(url)
+        return data.json()
+    except Exception as e:
+        print(f"could not get the url check if you typed the right currency {e}")
+        logger.info(f"error {e}")
 
-        if msg["text"] != 'Please don\'t quote me':
-            self.send_text_with_quote(msg['chat']['id'], msg["text"], quoted_msg_id=msg["message_id"])
+
+def get_activity():
+    try:
+        url = "https://www.boredapi.com/api/activity?type=recreational"
+        res = requests.get(url)
+        return res.json()
+    except Exception as e:
+        logger.info(f"error has occurred {e}")
 
 
 class ObjectDetectionBot(Bot):
     def handle_message(self, msg):
+        logger.info(f'Incoming message: {msg}')
+
         if self.is_current_msg_photo(msg):
-            # Download the user photo
             photo_path = self.download_user_photo(msg)
             photo_key = os.path.basename(photo_path)
-
-            # Upload the photo to S3
+            self.download_user_photo(msg)
+            # TODO upload the photo to S3
             s3 = boto3.client('s3')
-            s3.upload_file(photo_path, "hamad-aws-project", photo_key)
-            logger.info(f"Photo uploaded successfully to S3 bucket: 'hamad-aws-project' with key: {photo_key}")
+            try:
+                s3.upload_file(photo_path, "hamad-bucket-docker-project", photo_key)
 
-            # Send a message to SQS for processing
-            message = {
-                'image': photo_key,
-                'chat_id': msg['chat']['id']
-            }
-            self.send_message_to_sqs(json.dumps(message))
-        elif self.custom_startswith(msg["text"], "pixabay:"):
-            # Handle Pixabay API request
-            obj = msg["text"][len("pixabay:"):]
-            url2 = f"http://pixabay:8082/getImage?imgName={obj}"
-            data2 = requests.get(url2).content
-            self.send_text(msg['chat']['id'], f'Your Photo from Pixabay API: {data2} \n')
+                print(
+                    f"Photo uploaded successfully to S3 bucket: {'hamad-bucket-docker-project'} with key: {photo_key}")
+            except Exception as e:
+                print(f"could not upload error : {e}")
+                logger.info(f"uploading to s3 has failed error : {e}")
+            # TODO send a job to the SQS queue
+
+            sqs = boto3.client("sqs")
+            queue_url = "https://sqs.eu-north-1.amazonaws.com/933060838752/hamad-aws-project-sqs"
+            try:
+                sqs.send_message(
+                    QueueUrl=queue_url,
+                    MessageBody=photo_key + "," + msg['chat']['id']
+                )
+            except Exception as e:
+                print(f"could not send message to sqs queue error : {e}")
+                logger.info(f"sending message to sqs queue has failed error : {e}")
+
+            # TODO send message to the Telegram end-user (e.g. Your image is being processed. Please wait...)
+            self.send_text(msg["chat"]["id"], "Your image is being processed. Please wait...")
+        elif msg["text"].lower().startswith("pixabay:"):
+            try:
+                image_url = get_photo(msg['text'])
+                if image_url.startswith("http"):  # Simple check to ensure it's a URL
+                    logger.info(f" ###############{msg}")
+                    self.send_text(msg['chat']['id'], f"Image URL: {image_url}")
+                else:
+                    self.send_text(msg['chat']['id'], "Failed to fetch image from Pixabay.")
+            except Exception as e:
+                logger.error(f"Cannot get photo: {e}")
+                self.send_text(msg['chat']['id'], "An error occurred while fetching the photo.")
+        elif msg['text'].lower().startswith("crypto:"):
+            try:
+                data = get_info_of_currency(msg['text'].lower()[7:])
+                self.send_text(msg['chat']['id'],
+                               f"data for {msg['text'][7:]} is :\n {json.dumps(data['data'], indent=4)}")
+            except Exception as e:
+                logger.info(f"error has occurred : {e}")
+        elif "bored" in msg['text'].lower():
+            try:
+                activity = get_activity()
+                self.send_text(msg['chat']['id'], activity['activity'])
+            except Exception as e:
+                logger.info(f"error fetching data {e}")
         else:
-            # Handle other messages
+            logger.info("helllloooooooo")
             super().handle_message(msg)
-
-    def custom_startswith(self, s, prefix):
-        return s.startswith(prefix)
-
-    def send_message_to_sqs(self, message_json):
-        try:
-            # Deserialize the JSON string to a dictionary
-            message = json.loads(message_json)
-
-            # Create an SQS client
-            sqs = boto3.client('sqs', region_name="eu-north-1")
-            # SQS queue URL TODO change the sqs url my sqs url or use rabbitmq or kafka 
-            queue_url = 'https://sqs.eu-north-1.amazonaws.com/933060838752/hamad-aws-project-sqs'
-
-            # Add MessageGroupId parameter
-            response = sqs.send_message(
-                QueueUrl=queue_url,
-                MessageBody=message_json,  # Send the original JSON string as message body                  
-            )
-
-            logger.info("Message successfully sent to SQS: %s", response['MessageId'])
-            # Send a success message back to the user
-            #self.send_text(message['chat_id'], 'Message successfully sent to SQS \n')
-        except (ClientError, json.JSONDecodeError) as e:
-            logger.error("Error sending message to SQS: %s", e)
-            # Send an error message back to the user
-            #self.send_text(message['chat_id'], f'Error sending message to SQS: {str(e)} \n')
-
-
-
-
-
